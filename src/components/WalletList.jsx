72a3a4e2108c358924785d485211994dee1ecb00
Lucas Lisboa <lucas@pop-os.localdomain>
2022-03-09 00:02:21 -0300

    refazendo a table

--- src/components/WalletList.jsx
+++ src/components/WalletList.jsx
@@ -7,48 +7,57 @@ class WalletList extends Component {
   render() {
     const { expenses, dispatch } = this.props;
     return (
-      <tfoot>
-        <tr>
-          <th>Descrição </th>
-          <th>Moeda</th>
-          <th>Valor</th>
-          <th>Moeda de conversão</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Método de pagamento</th>
-          <th>Tag</th>
-          <th>Editar/Excluir</th>
-        </tr>
-        {expenses
-          .map(({ id, description, value, currency, method, tag, exchangeRates }) => (
-            <tr key={ id }>
-              <td>{description}</td>
-              <td>{exchangeRates[currency].name}</td>
-              <td>{(Number(value)).toFixed(2)}</td>
-              <td>Real</td>
-              <td>{Number(exchangeRates[currency].ask).toFixed(2)}</td>
-              <td>{(Number(value) * Number(exchangeRates[currency].ask)).toFixed(2)}</td>
-              <td>{method}</td>
-              <td>{tag}</td>
-              <td>
-                <button
-                  type="button"
-                  onClick={ () => dispatch(startEditItem(id)) }
-                  data-testid="edit-btn"
-                >
-                  Editar
-                </button>
-                <button
-                  type="button"
-                  onClick={ () => dispatch(removeItem(id)) }
-                  data-testid="delete-btn"
-                >
-                  Exclui
-                </button>
-              </td>
-            </tr>
-          ))}
-      </tfoot>
+      <table>
+
+        <thead>
+
+          <tr>
+            <th>Descrição </th>
+            <th>Moeda</th>
+            <th>Valor</th>
+            <th>Moeda de conversão</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Método de pagamento</th>
+
+            <th>Tag</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses
+            .map(({ id, description, value, currency, method, tag, exchangeRates }) => (
+              <tr key={ id }>
+                <td>{description}</td>
+                <td>{exchangeRates[currency].name}</td>
+                <td>{(Number(value)).toFixed(2)}</td>
+                <td>Real</td>
+                <td>{Number(exchangeRates[currency].ask).toFixed(2)}</td>
+                <td>
+                  {(Number(value) * Number(exchangeRates[currency].ask)).toFixed(2)}
+                </td>
+                <td>{method}</td>
+                <td>{tag}</td>
+                <td>
+                  <button
+                    type="button"
+                    onClick={ () => dispatch(startEditItem(id)) }
+                    data-testid="edit-btn"
+                  >
+                    Editar
+                  </button>
+                  <button
+                    type="button"
+                    onClick={ () => dispatch(removeItem(id)) }
+                    data-testid="delete-btn"
+                  >
+                    Exclui
+                  </button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     );
   }
 }
